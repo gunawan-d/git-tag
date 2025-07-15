9719dcb0288add11bcdb26a144b71777bb19f012
Gunawan <gunawandwi255@gmail.com>
2025-07-15 10:55:21 +0700

    DevOps add: add jobs deploy version 0.0.8

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Gunawan ini Version 0.0.7" }');
+    res.send('{ "response": "Hello From Gunawan ini Version 0.0.8" }');
 });
 
 app.get('/will', function (req, res) {
